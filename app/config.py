abdaf2c1b126811c88709568169af9dbceec6f5b
Kunal9131 <kunal.stevesai@gmail.com>
2025-09-26 16:47:44 +0530

    config file change

--- app/config.py
+++ app/config.py
@@ -1,14 +1,18 @@
+import os
+from dotenv import load_dotenv
 from fastapi_mail import ConnectionConfig
 
+load_dotenv()
+
 mail_config = ConnectionConfig(
-    MAIL_USERNAME="nileshlinux01@gmail.com",
-    MAIL_PASSWORD= "zzec ytjd nngt xajj",#"bhtv mpnb ufhz ftup",
-    MAIL_FROM="nileshlinux01@gmail.com",
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com", 
-    MAIL_FROM_NAME="RFP Automation",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+    MAIL_STARTTLS=os.getenv("MAIL_STARTTLS") == "True",
+    MAIL_SSL_TLS=os.getenv("MAIL_SSL_TLS") == "True",
+    USE_CREDENTIALS=os.getenv("USE_CREDENTIALS") == "True",
+    VALIDATE_CERTS=os.getenv("VALIDATE_CERTS") == "True"
 )
